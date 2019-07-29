735df0f951c608fb7c02db5b86fae738106fd379
MaiUbeid <ubead95@gmail.com>
2019-07-29 18:09:25 +0300

    Deploy

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const router = require('./router');
 
-const port = 7000;
+const port = process.env.port || 7000;
 
 const server = http.createServer(router);
 server.listen(port, () => {
